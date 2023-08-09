af627abc97f11d87742c195ac5f4bbf384b77792
DatNguyen <vinhdat4999@gmail.com>
2023-08-10 00:46:02 +0700

    #comment: v1.5.0

--- src/main/java/com/blas/blaspaymentgateway/controller/ChargeController.java
+++ src/main/java/com/blas/blaspaymentgateway/controller/ChargeController.java
@@ -77,31 +77,6 @@ public class ChargeController {
   @Autowired
   protected BlasPaymentTransactionLogService blasPaymentTransactionLogService;
 
-  protected void sendReceiptEmail(BlasPaymentTransactionLog blasPaymentTransaction, String username,
-      String cardNumber, Charge charge) {
-    AuthUser authUser = authUserService.getAuthUserByUsername(username);
-    HtmlEmailRequest htmlEmailRequest = new HtmlEmailRequest();
-    htmlEmailRequest.setEmailTo(authUser.getUserDetail().getEmail());
-    htmlEmailRequest.setTitle(SUBJECT_EMAIL_RECEIPT);
-    htmlEmailRequest.setEmailTemplateName(PAYMENT_RECEIPT.name());
-    htmlEmailRequest.setData(Map.ofEntries(
-        Map.entry("email", authUser.getUserDetail().getEmail()),
-        Map.entry("phone", authUser.getUserDetail().getPhoneNumber()),
-        Map.entry("name", authUser.getUserDetail().getFirstName() + SPACE + authUser.getUserDetail()
-            .getLastName()),
-        Map.entry("transactionId", blasPaymentTransaction.getPaymentTransactionLogId()),
-        Map.entry("transactionTime", blasPaymentTransaction.getTransactionTime().toString()),
-        Map.entry("cardType", blasPaymentTransaction.getCardType()),
-        Map.entry("cardNumber", maskCardNumber(cardNumber)),
-        Map.entry("status", charge.getStatus().toUpperCase()),
-        Map.entry("description", charge.getDescription()),
-        Map.entry("amount", String.valueOf((double) (charge.getAmountCaptured()) / 100)),
-        Map.entry("currency", charge.getCurrency().toUpperCase())
-    ));
-    CardController.sendEmail(htmlEmailRequest, blasEmailConfiguration, jwtTokenUtil,
-        centralizedLogService, serviceName, isSendEmailAlert);
-  }
-
   protected static String genTransactionId(
       BlasPaymentTransactionLogService blasPaymentTransactionLogService, int lengthOfId) {
     String transactionId;
@@ -135,5 +110,30 @@ public class ChargeController {
 
   protected static String getReformattedAmount(Long amount, String currency) {
     return (double) (amount) / 100 + StringUtils.SPACE + currency.toUpperCase();
+  }
+
+  protected void sendReceiptEmail(BlasPaymentTransactionLog blasPaymentTransaction, String username,
+      String cardNumber, Charge charge) {
+    AuthUser authUser = authUserService.getAuthUserByUsername(username);
+    HtmlEmailRequest htmlEmailRequest = new HtmlEmailRequest();
+    htmlEmailRequest.setEmailTo(authUser.getUserDetail().getEmail());
+    htmlEmailRequest.setTitle(SUBJECT_EMAIL_RECEIPT);
+    htmlEmailRequest.setEmailTemplateName(PAYMENT_RECEIPT.name());
+    htmlEmailRequest.setData(Map.ofEntries(
+        Map.entry("email", authUser.getUserDetail().getEmail()),
+        Map.entry("phone", authUser.getUserDetail().getPhoneNumber()),
+        Map.entry("name", authUser.getUserDetail().getFirstName() + SPACE + authUser.getUserDetail()
+            .getLastName()),
+        Map.entry("transactionId", blasPaymentTransaction.getPaymentTransactionLogId()),
+        Map.entry("transactionTime", blasPaymentTransaction.getTransactionTime().toString()),
+        Map.entry("cardType", blasPaymentTransaction.getCardType()),
+        Map.entry("cardNumber", maskCardNumber(cardNumber)),
+        Map.entry("status", charge.getStatus().toUpperCase()),
+        Map.entry("description", charge.getDescription()),
+        Map.entry("amount", String.valueOf((double) (charge.getAmountCaptured()) / 100)),
+        Map.entry("currency", charge.getCurrency().toUpperCase())
+    ));
+    CardController.sendEmail(htmlEmailRequest, blasEmailConfiguration, jwtTokenUtil,
+        centralizedLogService, serviceName, isSendEmailAlert);
   }
 }
